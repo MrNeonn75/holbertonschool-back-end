d60bc05004ac1abb6b80c20d6a2f64adab62ea3f
MrNeonn75 <devneonn75@gmail.com>
2024-04-28 21:55:54 +0400

    0. Gather data from an API bug fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,29 +8,29 @@ import requests
 import sys
 
 if (__name__ == '__main__'):
-        user_id = int(sys.argv[1])
-        user_name = requests.get(
-                "https://jsonplaceholder.typicode.com/users/{:d}".format(
-                        user_id
-                )
-        ).json().get("name")
-        
-        todos = requests.get(
-                "https://jsonplaceholder.typicode.com/todos?userId={:d}"
-                .format(user_id)
-        ).json()
-        titles = [
-                todo.get("title") for todo in todos if todo.get(
-                        "completed"
-                ) is True
-        ]
-        total_num = len(todos)
-        done_num = len(titles)
-        string = "Employee {:s} is done with tasks({:d}/{:d}):".format(
-                user_name,
-                done_num,
-                total_num
+    user_id = int(sys.argv[1])
+    user_name = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{:d}".format(
+            user_id
         )
-        print(string)
-        for title in titles:
-                print("\t {:s}".format(title))
+    ).json().get("name")
+
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={:d}"
+        .format(user_id)
+    ).json()
+    titles = [
+        todo.get("title") for todo in todos if todo.get(
+            "completed"
+        ) is True
+    ]
+    total_num = len(todos)
+    done_num = len(titles)
+    string = "Employee {:s} is done with tasks({:d}/{:d}):".format(
+        user_name,
+        done_num,
+        total_num
+    )
+    print(string)
+    for title in titles:
+        print("\t {:s}".format(title))
